ca25d62b82514546012b2c2b7bf49d0a288bcab4
klrkescano <escano.klark@gmail.com>
2025-02-24 13:08:20 -0700

    add some features

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,8 @@
-import { Link } from 'expo-router';
-import { Text, View } from 'react-native';
+import React from 'react';
+import { Redirect } from 'expo-router';
 
-export default function Index() {
-  return (
-    <View className="justify-center items-center flex-1">
-      <Text className="text-cyan-600 text-3xl">Welcome to Fitspre</Text>
-      <Link href="/(auth)/signIn"> Go to Sign-in Page</Link>
-    </View>
-  );
-}
+const index = () => {
+  return <Redirect href="/(auth)/signIn" />;
+};
+
+export default index;
